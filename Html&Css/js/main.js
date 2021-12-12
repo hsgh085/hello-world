693f3fc863de73e0b8e61d831f7096068903dc29
hsgh085 <84820008+hsgh085@users.noreply.github.com>
2021-12-12 21:44:21 +0900

    Delete Html&Css directory

--- Html&Css/js/main.js
+++
@@ -1,6 +0,0 @@
-const loginToggleBtn = document.querySelector(".login__toggleBtn");
-const loginMenu = document.querySelector(".login__menu");
-
-loginToggleBtn.addEventListener("click", () => {
-  loginMenu.classList.toggle("active");
-});
